852fb79ad70a2550c823ac574474e26b77a69cfc
Marqae Morris <mamorris920@gmail.com>
2024-11-11 15:10:13 -0500

    initial deploy

--- server.js
+++ server.js
@@ -23,26 +23,44 @@ app.use(bodyParser.urlencoded({extended: true}))
 app.use(bodyParser.json())
 app.use(express.static('public'))
 
-app.get('/', (req, res) => {
-  db.collection('messages').find().toArray((err, result) => {
-    if (err) return console.log(err)
+app.get('/', (req, res) => {// default page happen on refresh
+  db.collection('messages').find().toArray((err, result) => {//go to db and finds all of the
+    if (err) return console.log(err)//reading the html 
     res.render('index.ejs', {messages: result})
   })
 })
-
-app.post('/messages', (req, res) => {
-  db.collection('messages').insertOne({name: req.body.name, msg: req.body.msg, thumbUp: 0, thumbDown:0}, (err, result) => {
-    if (err) return console.log(err)
+app.post('/messages', (req, res) => {// this is taking the form and sending it to the database
+  db.collection('messages').insertOne({name: req.body.name, msg: req.body.msg, thumbUp: 0}, (err, result) => {
+    if (err) return console.log(err)//^ this is telling the database to add one
     console.log('saved to database')
-    res.redirect('/')
+    res.redirect('/')//refrsh, back to homepage
   })
 })
 
-app.put('/messages', (req, res) => {
+app.put('/messagesthumbup', (req, res) => {//update put new info
+  console.log(req.body)
+  console.log('thumb Up', req.body.thumbUp)
   db.collection('messages')
   .findOneAndUpdate({name: req.body.name, msg: req.body.msg}, {
     $set: {
       thumbUp:req.body.thumbUp + 1
+    }
+  }, {
+    sort: {_id: -1},
+    upsert: true
+  }, (err, result) => {
+    if (err) return res.send(err)
+    res.send(result)
+  })
+})
+
+
+app.put('/messagesthumbdown', (req, res) => {//update put new info
+  console.log(req.body)
+  db.collection('messages')
+  .findOneAndUpdate({name: req.body.name, msg: req.body.msg}, {
+    $set: {
+      thumbUp:req.body.thumbUp - 1
     }
   }, {
     sort: {_id: -1},
